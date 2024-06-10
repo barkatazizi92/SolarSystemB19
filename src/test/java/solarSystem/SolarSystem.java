8454a2a2529c9f700ad9c8694d6970389305675a
barkatazizi92 <barkatmilad@gmail.com>
2024-06-10 15:09:47 -0400

    planet2 is done

--- src/test/java/solarSystem/SolarSystem.java
+++ src/test/java/solarSystem/SolarSystem.java
@@ -13,5 +13,10 @@ public class SolarSystem {
         fe.color="pink";
         fe.temp="23r5434";
         fe.size="Okm";
+
+        planet2 p2 = new planet2();
+        p2.color ="green";
+        p2.size ="1m";
+        p2.radius ="3m";
     }
 }
---
+++ src/test/java/solarSystem/planet2.java
@@ -0,0 +1,7 @@
+package solarSystem;
+
+public class planet2 {
+    String color;
+    String radius;
+    String size;
+}
